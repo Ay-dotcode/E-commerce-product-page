221de6878b980f0f74fb5b8eef60403f58e57fd8
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-13 22:08:46 +0300

    function to add active class

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,19 @@ const cartNo = main.querySelector('#cartNo');
 const plus = main.querySelector('#plus');
 const minus = main.querySelector('#minus');
 
+const mainImg = main.querySelector('#mainImg');
+const thumbnail = main.querySelector('#thumbnail');
+
 // Plus and minus functionality
 plus.addEventListener('click', () => cartNo.value++);
 minus.addEventListener('click', () => cartNo.value -= inalis(cartNo.value > 1) ? 1 : 0);
+
+thumbnail.childNodes.forEach(element => element.addEventListener('click', changeImg));
+
+function changeImg(e) {
+    thumbnail.childNodes.forEach(node => {
+        // console.log(node);
+        // node.classList.remove('active');
+    });
+    e.target.classList.add('active');
+}
