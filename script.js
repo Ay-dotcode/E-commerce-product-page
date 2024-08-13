1afa0cd648e2e61e12624e95f46cbe07691a2d45
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-13 21:19:35 +0300

    adding the js for the plus and minus

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const main = document.querySelector('main');
+const cartNo = main.querySelector('#cartNo');
 const plus = main.querySelector('#plus');
 const minus = main.querySelector('#minus');
 
-console.log(plus, minus);
+plus.addEventListener('click', increment);
+minus.addEventListener('click', decrement);
+
+function increment() {
+    cartNo.value++;
+}
+function decrement() {
+    if (cartNo.value > 1)
+        cartNo.value--;
+}
