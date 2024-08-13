ae0cc41d46cc319667e74542915ab1d895c5eca4
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-13 23:06:38 +0300

    removing active only when clicking on an img

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,11 +14,11 @@ minus.addEventListener('click', () => cartNo.value -= (cartNo.value > 1) ? 1 : 0
 
 thumbnail.addEventListener('click', changeImg);
 function changeImg(e) {
-    thumbnail.childNodes.forEach(element => {
-        if (element.tagName === 'IMG')
-            element.classList.remove('active');
-    });
     if (e.target.tagName === 'IMG') {
+        thumbnail.childNodes.forEach(element => {
+            if (element.tagName === 'IMG')
+                element.classList.remove('active');
+        });
         e.target.classList.add('active');
         mainImg.src = `/assets/images/image-product-${e.target.alt[8]}.jpg`;
     }
