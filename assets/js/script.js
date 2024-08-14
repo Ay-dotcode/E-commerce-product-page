942e266e17b94750c2a17666c933d03939752601
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-14 14:51:40 +0300

    styling cart img

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,4 +25,4 @@ cart.addEventListener('click', () => openCart(main));
 // Add to cart functionality
 addCArt.addEventListener('click', () => addTocart(main, cartNo.value));
 
-// addTocart(main, cartNo.value);
+addTocart(main, cartNo.value);
