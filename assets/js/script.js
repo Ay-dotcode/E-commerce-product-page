970d83fa9157f6ce32ac567afd041334fb85f5a6
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-14 15:11:24 +0300

    stop adding by default

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,5 +24,3 @@ cart.addEventListener('click', () => openCart(main));
 
 // Add to cart functionality
 addCArt.addEventListener('click', () => addTocart(main, cartNo.value));
-
-addTocart(main, cartNo.value);
