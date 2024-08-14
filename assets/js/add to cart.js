0bf96930d998ceb0e0bd2355aac08a22fd458bd6
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-14 15:10:43 +0300

    adding the obj through js

--- assets/js/add to cart.js
+++ assets/js/add to cart.js
@@ -9,8 +9,19 @@ function addTocart(main, cartNo) {
     const cartEmpty = main.querySelector('#cartEmpty');
     const cartContainer = main.querySelector('#cartContainer');
 
-    //code here
-
+    const obj = document.createElement('section');
+    obj.id = "cartObj";
+    obj.innerHTML += `
+          <div class="row">
+            <img id="cartImg" src="/assets/images/image-product-1-thumbnail.jpg" alt="product 1">
+            <div>
+              <p id="prodName"> Fall Limited Edition Sneakers</p>
+              <p id="totalPrice"> $125.00 x ${cartNo} <span>$${125 * cartNo}</span></p>
+            </div>
+            <img id="delete" src="/assets/images/icon-delete.svg" alt="delete">
+          </div>
+          <button type="button" id="checkout">Checkout</button>`;
+    cartContainer.appendChild(obj);
     cartEmpty.style.display = 'none';
     openCart(main);
 }
