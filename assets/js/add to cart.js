1633951d9edadb51701031c07f82c586589ea13c
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-14 14:07:10 +0300

    adding js for cart screen

---
+++ assets/js/add to cart.js
@@ -0,0 +1,11 @@
+function addTocart(main, cartNo) {
+    const cartScreen = main.querySelector('#cartScreen');
+    const cartContainer = main.querySelector('#cartContainer');
+
+    //code here
+
+    cartScreen.style.display = 'block';
+}
+
+export { addTocart };
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,21 @@
+import { addTocart } from './add to cart.js';
 import { changeImg } from './change img.js';
 
 const main = document.querySelector('main');
-const cartNo = main.querySelector('#cartNo');
+
 const plus = main.querySelector('#plus');
 const minus = main.querySelector('#minus');
-
+const cartNo = main.querySelector('#cartNo');
+const addCArt = main.querySelector('#addCArt');
 const thumbnail = main.querySelector('#thumbnail');
+
 
 // Plus and minus functionality
 plus.addEventListener('click', () => cartNo.value++);
 minus.addEventListener('click', () => cartNo.value -= (cartNo.value > 1) ? 1 : 0);
+
+// Change image functionality
 thumbnail.addEventListener('click', changeImg);
+
+// Add to cart functionality
+addCArt.addEventListener('click', () => addTocart(main, cartNo.value));
