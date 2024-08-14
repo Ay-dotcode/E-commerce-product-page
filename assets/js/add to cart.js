d15b1a0ad3d58448006f4dda5a2b4978f954b3ea
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-14 15:26:20 +0300

    making ip only one checkout box

--- assets/js/add to cart.js
+++ assets/js/add to cart.js
@@ -1,17 +1,19 @@
 function openCart(main) {
-    const cartScreen = main.querySelector('#cartScreen');
-    if (cartScreen.style.display === 'none' || cartScreen.style.display === '')
-        cartScreen.style.display = 'block';
-    else cartScreen.style.display = 'none';
+  const cartScreen = main.querySelector('#cartScreen');
+  if (cartScreen.style.display === 'none' || cartScreen.style.display === '')
+    cartScreen.style.display = 'block';
+  else cartScreen.style.display = 'none';
 }
 
 function addTocart(main, cartNo) {
-    const cartEmpty = main.querySelector('#cartEmpty');
-    const cartContainer = main.querySelector('#cartContainer');
+  const checkout = main.querySelector('#checkout');
+  const cartEmpty = main.querySelector('#cartEmpty');
+  const cartScreen = main.querySelector('#cartScreen');
+  const cartContainer = main.querySelector('#cartContainer');
 
-    const obj = document.createElement('section');
-    obj.id = "cartObj";
-    obj.innerHTML += `
+  const obj = document.createElement('section');
+  obj.id = "cartObj";
+  obj.innerHTML += `
           <div class="row">
             <img id="cartImg" src="/assets/images/image-product-1-thumbnail.jpg" alt="product 1">
             <div>
@@ -19,10 +21,12 @@ function addTocart(main, cartNo) {
               <p id="totalPrice"> $125.00 x ${cartNo} <span>$${125 * cartNo}.00</span></p>
             </div>
             <img id="delete" src="/assets/images/icon-delete.svg" alt="delete">
-          </div>
-          <button type="button" id="checkout">Checkout</button>`;
-    cartContainer.appendChild(obj);
-    cartEmpty.style.display = 'none';
+          </div>`;
+  checkout.style.display = 'block';
+  cartContainer.insertBefore(obj, cartContainer.lastElementChild);
+  cartEmpty.style.display = 'none';
+
+  if (cartScreen.style.display !== 'block')
     openCart(main);
 }
 
