b5ab160081edaae34aa0f9c7a28dbb82d5b311ca
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-14 14:32:40 +0300

    making the cart btns open the cart

--- assets/js/add to cart.js
+++ assets/js/add to cart.js
@@ -1,13 +1,19 @@
+function openCart(main) {
+    const cartScreen = main.querySelector('#cartScreen');
+    if (cartScreen.style.display === 'none' || cartScreen.style.display === '')
+        cartScreen.style.display = 'block';
+    else cartScreen.style.display = 'none';
+}
+
 function addTocart(main, cartNo) {
     const cartEmpty = main.querySelector('#cartEmpty');
-    const cartScreen = main.querySelector('#cartScreen');
     const cartContainer = main.querySelector('#cartContainer');
 
     //code here
 
     cartEmpty.style.display = 'none';
-    cartScreen.style.display = 'block';
+    openCart(main);
 }
 
-export { addTocart };
+export { addTocart, openCart };
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,11 @@
-import { addTocart } from './add to cart.js';
+import { addTocart, openCart } from './add to cart.js';
 import { changeImg } from './change img.js';
 
 const main = document.querySelector('main');
+const header = document.querySelector('header');
 
 const plus = main.querySelector('#plus');
+const cart = header.querySelector('#cart');
 const minus = main.querySelector('#minus');
 const cartNo = main.querySelector('#cartNo');
 const addCArt = main.querySelector('#addCArt');
@@ -17,7 +19,10 @@ minus.addEventListener('click', () => cartNo.value -= (cartNo.value > 1) ? 1 : 0
 // Change image functionality
 thumbnail.addEventListener('click', changeImg);
 
+// Show cart functionality
+cart.addEventListener('click', () => openCart(main));
+
 // Add to cart functionality
 addCArt.addEventListener('click', () => addTocart(main, cartNo.value));
 
-addTocart(main, cartNo.value);
+// addTocart(main, cartNo.value);
