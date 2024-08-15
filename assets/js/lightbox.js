366ea99ac3c93bdaec0b9e5c2d8e79b7f00fe82b
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-15 09:05:40 +0300

    setting lightbox first view

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -2,12 +2,20 @@ function openLightbox(img) {
     const lightbox = document.querySelector('#lightbox');
     const close = lightbox.querySelector('#close');
     const lightboxImg = lightbox.querySelector('#lightBoxImg');
+    const lightboxThumbnail = lightbox.querySelector('#lightboxThumbnail');
     lightboxImg.src = img.src;
-    console.log(lightboxImg.src);
-
+    lightboxThumbnail.childNodes.forEach((thumbnail) => {
+        if (thumbnail.tagName === 'DIV') {
+            thumbnail = thumbnail.querySelector('img');
+            thumbnail.classList.remove('active');
+            if (lightboxImg.src[50] === thumbnail.alt[8])
+                thumbnail.classList.add('active');
+        }
+    });
     lightbox.style.display = 'flex';
     close.addEventListener('click', () => lightbox.style.display = 'none');
 }
+
 
 
 export { openLightbox };
