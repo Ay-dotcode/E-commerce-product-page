e127d73ed49a6c9b8f2c2e8474a3693f2f0f83dc
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-15 09:21:31 +0300

    feat: Add click functionality to change images in lightbox

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -10,12 +10,21 @@ function openLightbox(img) {
             thumbnail.classList.remove('active');
             if (lightboxImg.src[50] === thumbnail.alt[8])
                 thumbnail.classList.add('active');
+            thumbnail.addEventListener('click', (e) => { lightboxChange(e, lightboxThumbnail, lightboxImg) });
         }
     });
     lightbox.style.display = 'flex';
     close.addEventListener('click', () => lightbox.style.display = 'none');
 }
-
+function lightboxChange(e, imgesDiv, lightboxImg) {
+    const images = imgesDiv.querySelectorAll('img');
+    images.forEach((element) => {
+        if (element.tagName === 'IMG')
+            element.classList.remove('active');
+    });
+    e.target.classList.add('active');
+    lightboxImg.src = `/assets/images/image-product-${e.target.alt[8]}.jpg`;
+}
 
 
 export { openLightbox };
