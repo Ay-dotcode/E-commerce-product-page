e728ae436c995b13d44208e6dca44d5f84ca2079
Ay-dotcode <joseyowolabi@gmail.com>
2024-08-17 15:56:56 +0300

    opening mobile menu

--- assets/js/mobile menu.js
+++ assets/js/mobile menu.js
@@ -1,5 +1,9 @@
-function mobileMenuOpen(darkbg) {
+function mobileMenuOpen(header) {
+    const darkbg = header.querySelector('#darkbg');
+    const mobileClose = header.querySelector('#mobileClose');
+
     darkbg.style.display = 'block';
+    mobileClose.addEventListener('click', () => darkbg.style.display = 'none');
 }
 export { mobileMenuOpen };
 
